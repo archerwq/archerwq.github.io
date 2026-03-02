7b094e62d953e204595fe9dcc5745e8ec5ae71d0
wangqiang4 <wangqiang4@kingsoft.com>
2026-03-02 10:43:24 +0800

    替换掉jquery by AI

--- js/custom.js
+++ js/custom.js
@@ -1,33 +1,54 @@
 /**
- * Main JS file for BlogInn behaviours
+ * Main JS file for BlogInn behaviours (vanilla JS, no jQuery)
  */
-
-/*globals jQuery, document */
-(function ($) {
+(function () {
     "use strict";
 
-    $(document).ready(function(){
-        // Responsive video embeds
-        $('.entry-content').fitVids();
+    function ready(fn) {
+        if (document.readyState !== "loading") {
+            fn();
+        } else {
+            document.addEventListener("DOMContentLoaded", fn);
+        }
+    }
 
-        // Navigation
-        $('#menu-toggle').click(function(){
-            var _this = $(this);
-            _this.toggleClass( 'toggled-on' ).attr('aria-expanded', _this.attr('aria-expanded') === 'false' ? 'true' : 'false');
-            $('.nav-menu').slideToggle();
-        });
-        $(window).bind('resize orientationchange', function() {
-            if ( $('#menu-toggle').is(':hidden') ) {
-                $('#menu-toggle').removeClass('toggled-on').attr('aria-expanded', 'false');
-                $('.nav-menu').removeAttr('style');
-            }
-        });
+    ready(function () {
+        var entryContent = document.querySelector(".entry-content");
+        if (entryContent && typeof window.fitVids === "function") {
+            window.fitVids(entryContent);
+        }
 
-        // Scroll to top
-        $('#top-link').on('click', function(e) {
-            $('html, body').animate({'scrollTop': 0});
-            e.preventDefault();
-        });
+        var menuToggle = document.getElementById("menu-toggle");
+        var navMenu = document.querySelector(".nav-menu");
+        if (menuToggle && navMenu) {
+            menuToggle.addEventListener("click", function () {
+                var expanded = this.getAttribute("aria-expanded") === "true";
+                this.setAttribute("aria-expanded", !expanded);
+                this.classList.toggle("toggled-on");
+                navMenu.classList.toggle("toggled-on");
+            });
+            window.addEventListener("resize", function () {
+                if (window.getComputedStyle(menuToggle).display === "none") {
+                    menuToggle.setAttribute("aria-expanded", "false");
+                    menuToggle.classList.remove("toggled-on");
+                    navMenu.classList.remove("toggled-on");
+                }
+            });
+            window.addEventListener("orientationchange", function () {
+                if (window.getComputedStyle(menuToggle).display === "none") {
+                    menuToggle.setAttribute("aria-expanded", "false");
+                    menuToggle.classList.remove("toggled-on");
+                    navMenu.classList.remove("toggled-on");
+                }
+            });
+        }
+
+        var topLink = document.getElementById("top-link");
+        if (topLink) {
+            topLink.addEventListener("click", function (e) {
+                window.scrollTo({ top: 0, behavior: "smooth" });
+                e.preventDefault();
+            });
+        }
     });
-
-}(jQuery));
+})();
--- js/plugins.js
+++ js/plugins.js
@@ -1,7 +1,61 @@
 /*
- * FitVids 1.1
- * Copyright 2013, Chris Coyier - http://css-tricks.com + Dave Rupert - http://daverupert.com
- * Credit to Thierry Koblentz - http://www.alistapart.com/articles/creating-intrinsic-ratios-for-video/
- * Released under the WTFPL license - http://sam.zoy.org/wtfpl/
-*/
-!function(t){"use strict";t.fn.fitVids=function(e){var i={customSelector:null};if(!document.getElementById("fit-vids-style")){var r=document.head||document.getElementsByTagName("head")[0],d=".fluid-width-video-wrapper{width:100%;position:relative;padding:0;}.fluid-width-video-wrapper iframe,.fluid-width-video-wrapper object,.fluid-width-video-wrapper embed {position:absolute;top:0;left:0;width:100%;height:100%;}",a=document.createElement("div");a.innerHTML='<p>x</p><style id="fit-vids-style">'+d+"</style>",r.appendChild(a.childNodes[1])}return e&&t.extend(i,e),this.each(function(){var e=["iframe[src*='player.vimeo.com']","iframe[src*='youtube.com']","iframe[src*='youtube-nocookie.com']","iframe[src*='kickstarter.com'][src*='video.html']","object","embed"];i.customSelector&&e.push(i.customSelector);var r=t(this).find(e.join(","));r=r.not("object object"),r.each(function(){var e=t(this);if(!("embed"===this.tagName.toLowerCase()&&e.parent("object").length||e.parent(".fluid-width-video-wrapper").length)){var i="object"===this.tagName.toLowerCase()||e.attr("height")&&!isNaN(parseInt(e.attr("height"),10))?parseInt(e.attr("height"),10):e.height(),r=isNaN(parseInt(e.attr("width"),10))?e.width():parseInt(e.attr("width"),10),d=i/r;if(!e.attr("id")){var a="fitvid"+Math.floor(999999*Math.random());e.attr("id",a)}e.wrap('<div class="fluid-width-video-wrapper"></div>').parent(".fluid-width-video-wrapper").css("padding-top",100*d+"%"),e.removeAttr("height").removeAttr("width")}})})}}(window.jQuery||window.Zepto);
+ * FitVids (vanilla JS)
+ * Responsive video embeds - wrap iframes in fluid-width wrapper.
+ * Inspired by FitVids by Chris Coyier & Dave Rupert.
+ */
+(function () {
+    "use strict";
+
+    var styleStr =
+        ".fluid-width-video-wrapper{width:100%;position:relative;padding:0;}" +
+        ".fluid-width-video-wrapper iframe,.fluid-width-video-wrapper object,.fluid-width-video-wrapper embed{" +
+        "position:absolute;top:0;left:0;width:100%;height:100%;}";
+
+    function ensureStyle() {
+        if (!document.getElementById("fit-vids-style")) {
+            var style = document.createElement("style");
+            style.id = "fit-vids-style";
+            style.textContent = styleStr;
+            (document.head || document.documentElement).appendChild(style);
+        }
+    }
+
+    var selectors = [
+        "iframe[src*='player.vimeo.com']",
+        "iframe[src*='youtube.com']",
+        "iframe[src*='youtube-nocookie.com']",
+        "iframe[src*='kickstarter.com'][src*='video.html']",
+        "object",
+        "embed",
+    ].join(",");
+
+    function fitVids(container) {
+        if (!container || !container.querySelector) return;
+        ensureStyle();
+        var nodes = container.querySelectorAll(selectors);
+        for (var i = 0; i < nodes.length; i++) {
+            var el = nodes[i];
+            if (el.tagName.toLowerCase() === "embed" && el.parentNode && el.parentNode.tagName && el.parentNode.tagName.toLowerCase() === "object") continue;
+            if (el.parentNode && el.parentNode.className && el.parentNode.className.indexOf("fluid-width-video-wrapper") !== -1) continue;
+            var height =
+                el.tagName.toLowerCase() === "object" || (el.getAttribute("height") && !isNaN(parseInt(el.getAttribute("height"), 10)))
+                    ? parseInt(el.getAttribute("height"), 10)
+                    : el.offsetHeight;
+            var width = !isNaN(parseInt(el.getAttribute("width"), 10))
+                ? parseInt(el.getAttribute("width"), 10)
+                : el.offsetWidth;
+            if (!width || !height) continue;
+            var ratio = (height / width) * 100;
+            if (!el.id) el.id = "fitvid" + Math.floor(999999 * Math.random());
+            var wrapper = document.createElement("div");
+            wrapper.className = "fluid-width-video-wrapper";
+            wrapper.style.paddingTop = ratio + "%";
+            el.parentNode.insertBefore(wrapper, el);
+            wrapper.appendChild(el);
+            el.removeAttribute("height");
+            el.removeAttribute("width");
+        }
+    }
+
+    window.fitVids = fitVids;
+})();
